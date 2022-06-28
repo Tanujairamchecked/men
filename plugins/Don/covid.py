1d611b54e5a9f597b028e3fa521b3f8e39731b16
BalamuruganDV <102686521+BalamuruganDV@users.noreply.github.com>
2022-06-28 17:13:12 +0530

    Update covid.py

--- plugins/Don/covid.py
+++ plugins/Don/covid.py
@@ -12,7 +12,7 @@ BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton("𝙲𝙻𝙾𝚂𝙴", ca
 async def reply_info(client, message):
     query = message.text.split(None, 1)[1]
     await message.reply_video(
-        video="https://telegra.ph/file/d6d58617a459dad037bb2.mp4",
+        video="https://telegra.ph/file/1d4f32539872c80b2ae76.mp4",
         caption=covid_info(query),
         quote=True
     )
